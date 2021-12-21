abd33d737bc2a681a48dbf82718dabf8080db3f1
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-21 09:43:01 +0400

    create error.svelte component in lib to display supabase errors

---
+++ src/lib/Error.svelte
@@ -0,0 +1,9 @@
+<script>
+  // supabase returns errors that have a message we can show users
+  export let error
+</script>
+
+
+{#if condition}
+  <strong class="text-red-600">{error.message}</strong>
+{/if}
