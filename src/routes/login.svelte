2210b4d1ec4e9d972438bc5c3400facde4071d57
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-23 12:46:05 +0400

    create login route, add js imports and redirect logic

---
+++ src/routes/login.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { browser } from '$app/env'
+  import { goto } from '$app/navigation'
+  import { getUser, signIn } from '$lib/services'
+  import Error from '$lib/Error.svelte'
+
+  // redirect if already signed in
+  const user = getUser()
+  if (browser && user) goto('/')
+</script>
