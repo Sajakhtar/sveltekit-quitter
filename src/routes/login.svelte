9c1a56d10c9f6566c77c2edb61e4ad2d6c164861
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-24 17:52:49 +0400

    add daisy form with email input field only for magic link for login route

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -7,4 +7,32 @@
   // redirect if already signed in
   const user = getUser()
   if (browser && user) goto('/')
+
+  let email = "test@gmail.com"
+
+  let signInPromise = Promise.resolve({})
+  function handleSignIn() {
+    signInPromise = signIn({email})
+  }
 </script>
+
+
+{#await signInPromise}
+  Sending magic link to {email}
+{:then {data, error}}
+  <Error {error}/>
+
+  {#if data}
+    <strong class="text-green-600">Successfully sent magic link to {email}</strong>
+  {:else}
+    <form class="form-control" on:submit|preventDefault={handleSignIn}>
+      <label for="email" class="label">
+        <span class="label-text text-4xl">Login to Quitter</span>
+      </label>
+      <div class="relative">
+        <input bing:bind:value={email} id="email" type="email" placeholder="Email" required class="w-full pr-16 input input-primary input-bordered">
+        <button class="absolute top-0 right-0 rounded-l-none btn btn-primary">Get Magic Link</button>
+      </div>
+    </form>
+  {/if}
+{/await}
