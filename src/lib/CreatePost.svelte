ad8be05e933d80bdb89c9e38364274b39ce53720
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-26 11:14:36 +0400

    create a createPost component, add html form and button

---
+++ src/lib/CreatePost.svelte
@@ -0,0 +1,7 @@
+<form class="form-control" on:submit|preventDefault={handleCreatePost}>
+  <label class="label">
+    <span class="label-text">Your bio</span>
+  </label>
+  <textarea class="textarea h-24 textarea-bordered" placeholder="Bio"></textarea>
+  <button class="btn">neutral</button>
+</form>
