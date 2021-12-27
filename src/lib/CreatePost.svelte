8cafb46af37ed2d87032000b1fe189237fa30d66
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-27 14:28:03 +0400

    update CreatePost component with handleCreatePost function and createPostPromise

--- src/lib/CreatePost.svelte
+++ src/lib/CreatePost.svelte
@@ -1,7 +1,14 @@
 <script>
-  import { createPost } from '$lib/services'
+  import Error from '$lib/Error.svelte'
+  import { createPost, getUser } from '$lib/services'
 
   let postContent = ''
+
+  let createPostPromise = Promise.resolve({})
+  function handleCreatePost() {
+    createPostPromise = createPost({user: getUser().email, content: postContent})
+  }
+
 </script>
 
 
@@ -9,6 +16,17 @@
   <label for="post" class="label">
     <span class="label-text text-xl">What would you like to post?</span>
   </label>
-  <textarea bind:value={postContent} id="post" class="textarea h-24 textarea-bordered"></textarea>
-  <button class="btn">Post it</button>
+  <textarea bind:value={postContent} id="post" required class="textarea h-24 textarea-bordered"></textarea>
+
+  {#await createPostPromise}
+    <button disabled type="button" class="btn">Post it</button>
+  {:then {data, error}}
+    <button class="btn">Post it</button>
+    {#if data}
+      <!-- can put data in a component, similar to Error -->
+      <strong class="text-green-500">Successfully created post</strong>
+    {/if}
+    <Error {error} />
+  {/await}
+
 </form>
