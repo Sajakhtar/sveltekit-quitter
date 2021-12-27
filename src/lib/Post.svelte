a61f97bbf975e72471ed2374012f3a108ee4f912
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-27 15:12:13 +0400

    add daisyUI card in Post component

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -1,3 +1,25 @@
 <script>
+  export let id, user, content, likes = 0, comments = [], publicURL = null
 
 </script>
+
+
+
+<article class="card shadow-sm bg-accent text-accent-content">
+
+  {#if publicURL}
+    <figure>
+      <img src={publicURL}>
+    </figure>
+  {/if}
+
+  <div class="card-body">
+    <h2 class="card-title">{user} says...</h2>
+    <p>{content}</p>
+
+    <div class="card-actions">
+      <button class="btn btn-secondary">More info</button>
+    </div>
+  </div>
+
+</article>
