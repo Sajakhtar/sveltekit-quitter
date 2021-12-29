c6463e11a24ff82770a25b93e9df2fd46e0d433f
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-29 15:19:17 +0400

    implement signout() serrvice within index and redirect to /login if non-signedin user access '/'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,16 +15,28 @@
 
 
 <script>
+  import { browser } from '$app/env'
+  import { goto } from '$app/navigation'
   import CreatePost from '$lib/CreatePost.svelte'
   import Post from '$lib/Post.svelte'
   import Error from '$lib/Error.svelte'
+  import {signOut, getUser} from '$lib/services'
 
   export let posts, error
+
+  // redirect to login if there is not a current user
+  const user = getUser()
+  if (browser && !user) goto('/login')
+
+  async function handleSignout() {
+    await signOut()
+    goto('/login')
+  }
 </script>
 
 <header class="flex justify-between">
   <h1 class="text-2xl">Quitter (a copy of Twitter)</h1>
-  <button class="btn btn-sm">Logout</button>
+  <button on:click={handleSignout} class="btn btn-sm">Logout</button>
 </header>
 
 <CreatePost />
