7dd6f67c3ca8c300ead8cf8a695d1304707dff1a
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-29 21:04:10 +0400

    create an UploadImage component

---
+++ src/lib/UploadImage.svelte
@@ -0,0 +1,12 @@
+<script>
+  // your script goes here
+  export let file
+
+  $: console.log(file)
+
+  function getFile(e) {
+    file = !!e.target.files.length && e.target.files[0]
+  }
+</script>
+
+<input on:change={getFile} class="input" type="file">
